3f2aaee62c29179e0ff1ecc91d6b09f3e6bc3fb8
Jose Alfredo Dzul Hau <70233261+JAlfredoDzulHau@users.noreply.github.com>
2022-01-26 02:22:43 -0600

    Update confirmacion.js

--- javascript/confirmacion.js
+++ javascript/confirmacion.js
@@ -1,15 +1,18 @@
-
-
-function confirmacion(e) {
-    if (confirm("¿Esta seguro que quiere salir?, perderá sus datos")) {
-        return true;
-
-    }else{
-        e.preventDefault();
-    }
-}
-let linkSalir = document.querySelectorAll(".salir");
-
-for(var i = 0; i < linkSalir.length; i++){
-    linkSalir[i].addEventListener('click', confirmacion);
-}
+$('.salir').on('click',function(e){
+    e.preventDefault();
+    const href = $(this).attr('href') 
+    Swal.fire({
+        position: 'top',
+        icon: 'question',
+        title: '¿Esta seguro que quiere salir?, perderá sus datos',
+        confirmButtonColor: '#3085d6',
+        showConfirmButton: true,
+        showCancelButton: true,
+        
+        }).then((result) => {
+            if (result.value) {
+                document.location.href = href;
+                
+            }
+        })
+ })
